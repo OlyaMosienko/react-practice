e1f6ca6cea15bce2b2bc994315d9987efad9649b
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-10-05 12:19:06 +0300

    Add practicle work on React and TypeScript

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AboutPage(){
+    return (
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque non beatae maxime enim unde provident suscipit necessitatibus! Aperiam accusantium nihil accusamus in. Adipisci pariatur, eligendi sit eum delectus illo non consequatur neque blanditiis in explicabo animi voluptas sint officia nulla eaque ratione incidunt dignissimos vitae voluptatem. Possimus excepturi delectus autem.</p>
+    )
+}
